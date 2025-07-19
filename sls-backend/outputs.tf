5e38979fd3d10a47ae0d6f105a9792134cfdacaa
Het Patel <hetp1815@gmail.com>
2025-07-19 21:18:13 +0530

    feat(cognito): add Cognito user pool, identity pool, and related outputs for user authentication

--- sls-backend/outputs.tf
+++ sls-backend/outputs.tf
@@ -16,3 +16,22 @@ output "lambda_function_arn" {
 output "api_endpoint" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
+
+output "user_pool_id" {
+  description = "The ID of the Cognito User Pool"
+  value       = aws_cognito_user_pool.this.id
+}
+
+output "user_pool_client_id" {
+  description = "The ID of the Cognito User Pool Client"
+  value       = aws_cognito_user_pool_client.this.id
+}
+
+output "user_pool_arn" {
+  description = "The ARN of the Cognito User Pool"
+  value       = aws_cognito_user_pool.this.arn
+}
+
+output "identity_pool_id" {
+  value = aws_cognito_identity_pool.this.id
+}
